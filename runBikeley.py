1da1dc970da801fae7472ec7fd27c5bb31e741a8
Shiva Raisinghani <shiva@Shivas-MacBook-Pro.local>
2017-11-28 21:13:15 -0800

    trying to make it work for everyone

--- runBikeley.py
+++ runBikeley.py
@@ -1,8 +1,10 @@
 import time
 import os
+import subprocess
+from subprocess import *
 def loop():
     # import testhtml
-    os.system('python testhtml.py')
+    subprocess.call('python testhtml.py', shell=True, cwd='/Users/shiva/desktop/bikeley')
 while True:
     loop()
     time.sleep(1)
